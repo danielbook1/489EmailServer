d41a408eee55d2802bbe4c48e7105181ca7e2cbd
daniel <daniel@bookfamily.com>
2024-04-09 13:13:46 -0700

    added basic tables for users and emails to database, updated gitignore to ignore .db files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3000;
 const db = new sqlite3.Database('./cmail.db');
@@ -6,9 +7,17 @@ const db = new sqlite3.Database('./cmail.db');
 // create table if needed
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS Users (
-            email TEXT PRIMARY KEY,
-            password TEXT NOT NULL)`
-    );
+            username TEXT PRIMARY KEY,
+            password TEXT NOT NULL
+        )`);
+
+    db.run(`CREATE TABLE IF NOT EXISTS Emails (
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            username TEXT NOT NULL,
+            FOREIGN KEY (username) REFERENCES Users(username),
+            PRIMARY KEY (email, username)
+        )`);
 });
 
 app.use(express.json());
