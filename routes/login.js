b0815c24b0d2cd32683eb6bf58e9855efd9b2a64
Vishal-Malireddi <122837735+Vishal-Malireddi@users.noreply.github.com>
2024-04-09 23:42:14 -0700

    Add files via upload

--- routes/login.js
+++ routes/login.js
@@ -6,26 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
         
       const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
-  
-      if (true) { //TODO: Add checks for valid username and password
-        // submit form data
-        fetch('/login', {
-          method: 'POST',
-          headers: {
-          'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({username, password})
-        })
-        .then(response => {
-            if (response.ok) {
-              // Redirect to login page
-              window.location.href = 'home.html';
-            } 
-            else {
-              console.error('Form submission failed');
-              // Handle errors
-            }
+      const captcha = document.getElementById('captcha').value;
+
+      if (captcha.length > 0)
+      {
+        if (true) { //TODO: Add checks for valid username and password
+          // submit form data
+          fetch('/login', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
           })
+          .then(response => {
+              if (response.ok) {
+                // Redirect to login page
+                window.location.href = 'home.html';
+              } 
+              else {
+                console.error('Form submission failed');
+                // Handle errors
+              }
+            })
+        }
       }
     });
 });
--- routes/newuser.js
+++ routes/newuser.js
@@ -6,26 +6,30 @@ document.addEventListener('DOMContentLoaded', function() {
         
       const username = document.getElementById('username').value;
       const password = document.getElementById('password').value;
+      const captcha = document.getElementById('captcha').value;
   
-      if (true) { //TODO: Add checks for valid username and password
-        // submit form data
-        fetch('/submitNewUser', {
-          method: 'POST',
-          headers: {
-          'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({username, password})
-        })
-        .then(response => {
-            if (response.ok) {
-              // Redirect to login page
-              window.location.href = 'loginPage.html';
-            } 
-            else {
-              console.error('Form submission failed');
-              // Handle errors
-            }
+      if (captcha.length > 0)
+      {
+        if (true) { //TODO: Add checks for valid username and password
+          // submit form data
+          fetch('/submitNewUser', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({username, password})
           })
+          .then(response => {
+              if (response.ok) {
+                // Redirect to login page
+                window.location.href = 'loginPage.html';
+              } 
+              else {
+                console.error('Form submission failed');
+                // Handle errors
+              }
+            })
+        }
       }
     });
 });
