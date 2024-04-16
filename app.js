d7ec186f97bd23e5f87e2149f9d9ca3a5bfe86ae
Vishal-Malireddi <122837735+Vishal-Malireddi@users.noreply.github.com>
2024-04-16 14:21:45 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const session = require('express-session');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const port = 3000;
 const db = new sqlite3.Database('./cmail.db');
 
+
 // create table if needed
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS Users (
             username TEXT PRIMARY KEY,
-            password TEXT NOT NULL
-        )`);
+            password TEXT NOT NULL,
+            admin TEXT NOT NULL
+            )`
+            );
+
+    //db.run(`INSERT INTO Users VALUES ('admin', 'value', 'yes')`)
 
     db.run(`CREATE TABLE IF NOT EXISTS Emails (
             email TEXT NOT NULL,
@@ -22,22 +28,41 @@ db.serialize(() => {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(express.static('views'));
-app.use(express.static('routes'));
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+const requireLogin = (req, res, next) => {
+    if (req.session && req.session.userId) {
+      // User is authenticated, proceed to the next middleware
+      next();
+    }
+    else {
+      // User is not authenticated, redirect to login page
+      res.sendFile(__dirname + '/views/loginPage.html');
+    }
+};
 
 // route to get HTML form
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/loginPage.html');
 });
 
+// routes that require user to be logged in
+app.get('/home.html', requireLogin, (req, res) => {
+    res.sendFile(__dirname + '/views/home.html');
+});
+
 // route to handle signup form
 app.post('/submitNewUser', (req, res) => {
     const {username, password} = req.body;
+    const isAdmin = "no"
 
     // Insert data into database
-    const sql = 'INSERT INTO Users (username, password) VALUES (?, ?)';
-    db.run(sql, [username, password], function(err) {
+    const sql = 'INSERT INTO Users (username, password, admin) VALUES (?, ?, ?)';
+    db.run(sql, [username, password, isAdmin], function(err) {
         if (err) {
             console.error(err.message);
             res.status(500).send('Internal Server Error');
@@ -65,6 +90,7 @@ app.post('/login', (req, res) => {
                 // If a user with the given username exists, check if the password matches
                 if (row.password === password) {
                     // Password matches, authentication successful
+                    req.session.userId = username;
                     res.status(200).send('User authentication successful');
                 } 
                 else {
@@ -79,7 +105,120 @@ app.post('/login', (req, res) => {
         }
     });
 });
-  
+
+//Logout route
+app.get('/logout', requireLogin, function(req, res, next) {
+    res.sendFile(__dirname + '/views/logout.html');
+});
+
+//Add email route
+app.get('/addEmail', requireLogin,  function(req, res, next) {
+    res.sendFile(__dirname + '/views/addEmail.html');
+});
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+//Add Admin route
+app.get('/admin',(req, res) => {
+  let sql;
+  //Opens the db
+  const db = new sqlite3.Database('cmail.db', (err)=>{
+    if(err) 
+      {
+        console.log("1")
+        return console.error(err)
+      }
+  })
+  console.log("DB Opened")
+
+  //Gets all the users to pass into the table in admin.ejs
+  db.all('SELECT * FROM Users', (err, rows) =>{
+    if(err){
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    console.log({Users: rows})
+    res.render('admin', {Users: rows});
+  })
+});
+
+//Admin post route
+app.post('/admin', (req, res) => {
+    const { username, password, formType } = req.body;
+    //Open up db
+    const db = new sqlite3.Database('cmail.db', (err)=>{
+        if(err) 
+        {
+            console.log("1")
+            return console.error(err)
+        }
+    })
+
+    //Action when promoteUser form is submitted
+    if(formType === 'promoteUser')
+    {
+        const promoteToAdmin = `UPDATE Users SET admin = ? WHERE username = ? AND password = ?`
+        const yes = 'yes'
+        db.run(promoteToAdmin, [yes, username, password], function(err) {
+            if (err) {
+              console.error('Error cannot promote to Admin: ', err.message);
+              return;
+            }
+            console.log('Value updated successfully.');
+          }); 
+        db.close();
+    }
+
+    //Action when deleteUser is submitted
+    else if(formType === 'deleteUser')
+    {
+        const promoteToAdmin = `DELETE FROM Users WHERE username = ? AND password = ?`
+        const yes = 'yes'
+        db.run(promoteToAdmin, [username, password], function(err) {
+            if (err) {
+              console.error('Error cannot delete user: ', err.message);
+              return;
+            }
+            console.log('User deleted successfully.');
+          }); 
+        db.close();
+    }
+
+    //Action for when logout is submitted
+    else if(formType === 'logOut')
+    {
+        res.status(200).send('User authentication successful');
+    }
+
+    let sql;
+    const db2 = new sqlite3.Database('cmail.db', (err)=>{
+        if(err) 
+        {
+            console.log("1")
+            return console.error(err)
+        }
+    })
+
+    db2.all('SELECT * FROM Users', (err, rows) =>{
+        if(err){
+        res.status(500).send('Internal Server Error');
+        return;
+        }
+        console.log({Users: rows})
+        res.render('admin', {Users: rows});
+    })
+});
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+
+
+app.use(express.static('public'));
+app.use(express.static('views'));
+app.use(express.static('routes'));
